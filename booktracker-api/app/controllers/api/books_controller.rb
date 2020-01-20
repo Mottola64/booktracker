ea43b1787943158e8b884fe57ffc0ce4cb67da3a
Michael Mottola <mottola64@gmail.com>
2020-01-20 15:24:58 -0500

    First Commit

---
+++ booktracker-api/app/controllers/api/books_controller.rb
@@ -0,0 +1,44 @@
+class Api::BooksController < ApplicationController
+
+    def index
+        
+        @books = Book.all
+
+        render json: @books, status: 200
+    end
+
+    def show
+
+        @book = Book.find(params[:id])
+
+        render json: @book, status: 200
+    end    
+
+    def create
+        @book = Book.create(book_params)
+
+        render json: @book, status: 200
+    end
+
+    def update
+        @book = Book.find(params[:id])
+        @book.update(book_params)
+
+        render json: @book, status: 200
+    end
+
+    def destroy
+        @book = Book.find(params[:id])
+        @book.delete
+
+        render json: {bookId: @book.id}
+
+    end
+
+    private
+        def book_params
+            params.require(:book).permit(:title, :author, :genre)
+        end
+
+
+end
